25e301a8919ad52919311807e1c2ebdda72ea1a0
Jofeu <jofeueve@gmail.com>
2021-05-16 07:36:29 +0530

    Initial Commit

---
+++ src/test/java/com/jose/jokesappnew/JokesAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.jokesappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
